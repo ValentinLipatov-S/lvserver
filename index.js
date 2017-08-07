6e58956efb1daae087d27c98ed925060bd73f4ad
ValentinLipatov <valentin-ltd@mail.ru>
2017-08-07 22:05:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,29 @@
-var express = require("express");
-var app = express();
-app.use(express.logger());
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    io.sockets.on('connection', function (socket) {
+        //var client = new Client(socket);
+        //clients.push(client);
+        console.log('CONNECTED');
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-  var PORT = 6969;
-  var io = require('socket.io').listen(PORT);
-  var clients = [];
-  //socket.io
-  io.sockets.on('connection', function (socket) {
-      var client = new Client(socket);
-      //clients.push(client);
-      console.log('CONNECTED');
+        socket.on('message', function (msg) {
+            console.log('WRITE');
+            //client.onMessage(msg);
+        });
 
-      socket.on('message', function (msg) {
-          console.log('WRITE');
-          //client.onMessage(msg);
-      });
+        socket.on('input', function (pos) {
+             console.log('INPUT');
+            //client.onInput(pos);
+        });
 
-      socket.on('input', function (pos) {
-           console.log('INPUT');
-          //client.onInput(pos);
-      });
-
-      socket.on('disconnect', function () {
-          console.log('CLOSED');
-          //var index = clients.indexOf(client);
-          //if (index != -1) {
-          //    clients.splice(index);
-          //}
-          client.onDisconnect();
-      });
-  });
-  console.log('Server started on port: ' + PORT);
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+        socket.on('disconnect', function () {
+            console.log('CLOSED');
+            //var index = clients.indexOf(client);
+            //if (index != -1) {
+            //    clients.splice(index);
+            //}
+            client.onDisconnect();
+        });
+    });
+server.listen(process.env.PORT || 3000);
