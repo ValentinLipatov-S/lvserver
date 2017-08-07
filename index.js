0eb3cf4988cd0cd5af8c24d84ee80681130940b8
ValentinLipatov <valentin-ltd@mail.ru>
2017-08-07 23:53:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ server.listen(9000, function() {
   console.log('Server listening to %j', server.address());
 });
 
- sock.on('connection', function(sock) {
+ server.on('connection', function(sock) {
     console.log('CONNECTED');
  });
 
-sock.on('data', function(data) {
+server.on('data', function(data) {
   console.log('DATA');
 });
 
-sock.on('close', function(data) {
+server.on('close', function(data) {
   console.log('CLOSED');
 });
