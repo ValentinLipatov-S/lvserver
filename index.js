1b1b9ec5c5d6a6efdfa813ef462c25bf73734cfa
ValentinLipatov <valentin-ltd@mail.ru>
2017-08-07 23:24:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,81 +5,21 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 
-
 var nextUserId = 0;
 
 var users = [];
 
-
-
 app.set('port', (process.env.PORT || 5000));
 
-
-
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-
+    //res.sendFile(__dirname + '/index.html');
+    res.send("Server is Working");
 });
-
-io.on('connection', function (socket) {
-
-    console.log(http);
-    var socketId = socket.id;
-
-    users.push({ 'id': socketId, 'name': "User" + nextUserId });
-
-    nextUserId++;
-
-    console.log(users[users.length - 1].name + ' joined with id ' + users[users.length - 1].id);
-
-    io.emit('user connected', users[users.length - 1].name);
-
-    socket.on('disconnect', function () {
-        console.log(app.get('port') + " " + app.get('ip'));
-        console.log('user disconnected');
-
-    });
-
-    socket.on('chat message', function (msg) {
-
-        var name;
-
-        for (var x = 0; x < users.length; x++) {
-
-            if (users[x].id == socket.id) {
-
-                name = users[x].name;
-
-            }
-
-        }
-
-
-
-        io.emit('chat message', name + ": " + msg);
-
-        console.log('message: ' + name + ": " + msg);
-
-    });
-
-});
-
-http.listen(app.get('port'), function(){
-
-    console.log('listening on port ' + app.get('port'));
-
-});
-
-
-
-
-
-
 
 var net = require('net');
 
 var HOST = '127.0.0.1';
-var PORT = 6969;
+var PORT = process.env.PORT;
 
 // Create a server instance, and chain the listen function to it
 // The function passed to net.createServer() becomes the event handler for the 'connection' event
