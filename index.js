669f35afdc75d8ad8aa4985e1cca97c626de3a64
ValentinLipatov <valentin-ltd@mail.ru>
2017-08-07 23:52:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
 var net = require('net');
-
-var server = net.createServer();  
-server.on('connection', handleConnection);
+var server = net.createServer();
 
 server.listen(9000, function() {  
   console.log('server listening to %j', server.address());
 });
 
-function handleConnection(conn) {  
-  var remoteAddress = conn.remoteAddress + ':' + conn.remotePort;
-  console.log('new client connection from %s', remoteAddress);
+ sock.on('connection', function(sock) {
+    console.log('CONNECTED');
+ });
 
-  conn.on('data', onConnData);
-  conn.once('close', onConnClose);
-  conn.on('error', onConnError);
+sock.on('data', function(data) {
+  console.log('DATA');
+});
 
-  function onConnData(d) {
-    console.log('connection data from %s: %j', remoteAddress, d);
-    conn.write(d);
-  }
-
-  function onConnClose() {
-    console.log('connection from %s closed', remoteAddress);
-  }
-
-  function onConnError(err) {
-    console.log('Connection %s error: %s', remoteAddress, err.message);
-  }
-}
+sock.on('close', function(data) {
+  console.log('CLOSED');
+});
