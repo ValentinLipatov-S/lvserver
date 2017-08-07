a7b32e133a48de7c64a4307271c487bc6504eeb0
ValentinLipatov <valentin-ltd@mail.ru>
2017-08-07 22:37:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ app.set('port', (process.env.PORT || 5000));
 
 
 app.get('/', function(req, res){
-    res.send(process.env);
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html?port=' + process.env.PORT);
 
 });
 
