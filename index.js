4717e01eb64aab3593125718a91868691f064a81
ValentinLipatov <valentin-ltd@mail.ru>
2017-08-07 21:52:34 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+var PORT = 6969;
+var io = require('socket.io').listen(PORT);
+var clients = [];
+//socket.io
+io.sockets.on('connection', function (socket) {
+    var client = new Client(socket);
+    //clients.push(client);
+    console.log('CONNECTED');
+
+    socket.on('message', function (msg) {
+        console.log('WRITE');
+        //client.onMessage(msg);
+    });
+
+    socket.on('input', function (pos) {
+         console.log('INPUT');
+        //client.onInput(pos);
+    });
+
+    socket.on('disconnect', function () {
+        console.log('CLOSED');
+        //var index = clients.indexOf(client);
+        //if (index != -1) {
+        //    clients.splice(index);
+        //}
+        client.onDisconnect();
+    });
+});
+
+console.log('Server started on port: ' + PORT);
