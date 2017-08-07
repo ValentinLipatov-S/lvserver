137bba92ed8b6ccfa793af7fc85ac1304f6394d7
ValentinLipatov <valentin-ltd@mail.ru>
2017-08-07 22:01:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,35 @@ app.use(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
+  var PORT = 6969;
+  var io = require('socket.io').listen(PORT);
+  var clients = [];
+  //socket.io
+  io.sockets.on('connection', function (socket) {
+      var client = new Client(socket);
+      //clients.push(client);
+      console.log('CONNECTED');
+
+      socket.on('message', function (msg) {
+          console.log('WRITE');
+          //client.onMessage(msg);
+      });
+
+      socket.on('input', function (pos) {
+           console.log('INPUT');
+          //client.onInput(pos);
+      });
+
+      socket.on('disconnect', function () {
+          console.log('CLOSED');
+          //var index = clients.indexOf(client);
+          //if (index != -1) {
+          //    clients.splice(index);
+          //}
+          client.onDisconnect();
+      });
+  });
+  console.log('Server started on port: ' + PORT);
 });
 
 var port = process.env.PORT || 5000;
