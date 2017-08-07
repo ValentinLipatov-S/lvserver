4512e2ca4f6c702bc60c415c05ef127fb747835b
ValentinLipatov <valentin-ltd@mail.ru>
2017-08-08 00:49:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,16 @@
-var sys = require('sys');
 var net = require('net');
-var sockets = [];
  
 var svr = net.createServer(function(sock) {
-    sys.puts('Connected: ' + sock.remoteAddress + ':' + sock.remotePort); 
-    sock.write('Hello ' + sock.remoteAddress + ':' + sock.remotePort + '\n');
-    sockets.push(sock);
- 
-    sock.on('data', function(data) {  // client writes message
-        if (data == 'exit\n') {
-            sys.puts('exit command received: ' + sock.remoteAddress + ':' + sock.remotePort + '\n');
-            sock.destroy();
-            var idx = sockets.indexOf(sock);
-            if (idx != -1) {
-                delete sockets[idx];
-            }
-            return;
-        }
-        var len = sockets.length;
-        for (var i = 0; i < len; i ++) { // broad cast
-            if (sockets[i] != sock) {
-                if (sockets[i]) {
-                    sockets[i].write(sock.remoteAddress + ':' + sock.remotePort + ':' + data);
-                }
-            }
-        }
+    console.log('CONNECT');
+    sock.on('data', function(data) { console.log('MESSAGE');
     });
  
-    sock.on('end', function() { // client disconnects
-        sys.puts('Disconnected: ' + data + data.remoteAddress + ':' + data.remotePort + '\n');
-        var idx = sockets.indexOf(sock);
-        if (idx != -1) {
-            delete sockets[idx];
-        }
+    sock.on('end', function() { console.log('DISCONNECT');
     });
 });
  
 var svraddr = '127.0.0.1';
 var svrport = (process.env.PORT || 5000);
  
-svr.listen(svrport, svraddr);
-sys.puts('Server Created at ' + svraddr + ':' + svrport + '\n');
+svr.listen(svrport);
+console.log('Server Created at ' + svraddr + ':' + svrport + '\n');
