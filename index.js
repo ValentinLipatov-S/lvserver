59b52c17a3e54377d63539b8e9b1b78bdebcb86c
ValentinLipatov <valentin-ltd@mail.ru>
2017-08-07 23:59:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var net = require('net');
 var server = net.createServer();
 
-server.listen(9000, function() {  
+server.listen(80, function() {  
   console.log('Server listening to %j', server.address());
 });
 
