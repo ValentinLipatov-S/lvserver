c653e56b32d6a5ec50125dab16815344a6c87ab8
ValentinLipatov <valentin-ltd@mail.ru>
2017-08-07 22:06:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
+    io = require('socket.io').listen(server);
     io.sockets.on('connection', function (socket) {
         //var client = new Client(socket);
         //clients.push(client);
