fe3a5c9b4339ec87302fb663c70054b11fff3b58
ValentinLipatov <valentin-ltd@mail.ru>
2017-08-08 00:39:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ net.createServer(function(sock) {
 
     // We have a connection - a socket object is assigned to the connection automatically
     console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
-    sock.write('Hello');
     // Add a 'data' event handler to this instance of socket
     sock.on('data', function(data) {
         console.log('DATA ' + sock.remoteAddress + ': ' + data);
