1a476a82961e1ebee6c0b7a906c8e97b4e73c2db
ValentinLipatov <valentin-ltd@mail.ru>
2017-08-08 18:02:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const INDEX = path.join(__dirname, 'index.html');
 
 var clients = {};
 
+var chatroom = {
+	clients: {}
+};
+var chatrooms = {};
+
+
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
@@ -24,8 +30,35 @@ io.on('connection', function (socket) {
     if(clients[socket.id.toString()] != undefined) delete clients[socket.id.toString()];
     console.log('Client disconnected'); });
   /*MESSAGE*/
-  socket.on('message', function (msg) { 
-    for(var key in clients) clients[key].json.send({'comand': 'message', 'user_id': socket.id.toString(), 'text': msg}); 
+  socket.on('message', function (msg) {
+	  
+    if(msg['comand'] == 'add_to_chatroom') {
+    	if(chatrooms[msg['chatroom_name']].clients.indexOf(msg['user_id']) == -1) {
+		chatrooms[msg['chatroom_name']].clients.push(msg['user_id']);
+    }
+    if(msg['comand'] == 'kick_from_chatroom') {
+	var index = chatrooms[msg['chatroom_name']].clients.indexOf(msg['user_id']);
+    	if(index > -1) {
+		chatrooms[msg['chatroom_name']].clients.slice(index, 1);
+	}
+    }    
+    if(msg['comand'] == 'create_chatroom') {
+	    if(chatrooms[msg['chatroom_name']] == undefined) {
+		    var this_chatroom = chatroom;
+		    this_chatroom.clients.push(socket.id.toString());
+		    chatrooms[msg['chatroom_name']] = this_chatroom;
+	    }
+    }
+   if(msg['comand'] == 'send_to_chatroom') {
+	   var current_chatroom = chatrooms[msg['chatroom_name']];
+	   if(current_chatroom != undefined) {
+   		for(var i = 0; i < current_chatroom.clients.length; i++)
+		{
+			clients[current_chatroom.clients[i]].send('message');
+		}
+	   }
+   }	    
+    //for(var key in clients) clients[key].json.send({'comand': 'message', 'user_id': socket.id.toString(), 'text': msg}); 
     //console.log('Client send messgae ' + msg); 
   });
 });
